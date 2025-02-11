abcb77ccfb53b52c03956d2186908eb20738a8e5
Vladimir <scobca18@yandex.ru>
2025-02-12 02:24:00 +0300

    feat(core): run invoker in runtime

--- src/main/kotlin/org/example/kotlincollectionmanager/KotlinCollectionManagerApplication.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/KotlinCollectionManagerApplication.kt
@@ -1,13 +1,16 @@
 package org.example.kotlincollectionmanager
 
+import org.example.kotlincollectionmanager.invoker.InvokerService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinCollectionManagerApplication : CommandLineRunner {
+class KotlinCollectionManagerApplication(private val invokerService: InvokerService) : CommandLineRunner {
+
     override fun run(vararg args: String?) {
-        println("Kotlin Collection Manager started")
+        val filename: String? = System.getProperty("file")
+        invokerService.run()
     }
 }
 
