a71b3104742b40c12d01dfce399d9ff722377f39
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-02-26 00:20:02 +0300

    feat(house): created setters for fields

--- src/main/kotlin/org/example/kotlincollectionmanager/collection/items/House.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/collection/items/House.kt
@@ -1,13 +1,17 @@
 package org.example.kotlincollectionmanager.collection.items
 
 class House(
-    private var name: String,
-    private var year: Int,
-    private var numberOfFloors: Long
+    private var name: String? = null,
+    private var year: Int? = null,
+    private var numberOfFloors: Long? = null,
 ) {
-    fun getName(): String = name
-    fun getYear(): Int = year
-    fun getNumberOfFloors(): Long = numberOfFloors
+    fun getName(): String? = name
+    fun getYear(): Int? = year
+    fun getNumberOfFloors(): Long? = numberOfFloors
+
+    fun setName(name: String) = apply { this.name = name }
+    fun setYear(year: Int) = apply { this.year = year }
+    fun setNumberOfFloors(numberOfFloors: Long) = apply { this.numberOfFloors = numberOfFloors }
 
     override fun toString(): String {
         return "House(name='$name', year=$year, numberOfFloors=$numberOfFloors)"
