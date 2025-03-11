42f751748f638635d81cbe45fae099056851d26e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-11 22:46:35 +0300

    docs(javadoc): described json parser classes

--- src/main/kotlin/org/example/kotlincollectionmanager/parser/CoordinatesJson.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/parser/CoordinatesJson.kt
@@ -2,7 +2,17 @@ package org.example.kotlincollectionmanager.parser
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * A class for representing coordinates in JSON format.
+ * Used to deserialize coordinates from a JSON string.
+ */
 data class CoordinatesJson(
+    /**
+     * X coordinate.
+     */
     @JsonProperty("x") val x: Long? = null,
+    /**
+     * Y coordinate.
+     */
     @JsonProperty("y") val y: Float? = null,
 )
--- src/main/kotlin/org/example/kotlincollectionmanager/parser/FlatJson.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/parser/FlatJson.kt
@@ -2,14 +2,45 @@ package org.example.kotlincollectionmanager.parser
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * Class for representing apartments in JSON format.
+ * Used to deserialize apartment data from a JSON string.
+ */
 data class FlatJson(
+    /**
+     * Apartment ID.
+     */
     @JsonProperty("id") val id: Long? = null,
+    /**
+     * The name of the apartment.
+     */
     @JsonProperty("name") val name: String? = null,
+    /**
+     * Coordinates of the apartment.
+     */
     @JsonProperty("coordinates") val coordinates: CoordinatesJson? = null,
+    /**
+     * The area of the apartment.
+     */
     @JsonProperty("area") val area: Long? = null,
+    /**
+     * The number of rooms in the apartment.
+     */
     @JsonProperty("numberOfRooms") val numberOfRooms: Long? = null,
+    /**
+     * The price of the apartment.
+     */
     @JsonProperty("price") val price: Long? = null,
+    /**
+     * The presence of a balcony.
+     */
     @JsonProperty("balcony") val balcony: Boolean? = null,
+    /**
+     * The type of furniture in the apartment.
+     */
     @JsonProperty("furnish") val furnish: String? = null,
+    /**
+     * Information about the house where the apartment is located.
+     */
     @JsonProperty("house") val house: HouseJson? = null,
 )
