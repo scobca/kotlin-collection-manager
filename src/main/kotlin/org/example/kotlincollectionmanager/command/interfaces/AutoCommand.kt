b0060ecf8d7edb9fb3ba10452980d63c58357a53
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-11 18:57:14 +0300

    docs(javadoc): described command interfaces

--- src/main/kotlin/org/example/kotlincollectionmanager/command/interfaces/AutoCommand.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/command/interfaces/AutoCommand.kt
@@ -1,5 +1,17 @@
 package org.example.kotlincollectionmanager.command.interfaces
 
+/**
+ * Interface for commands that can be executed either manually or automatically.
+ * Extends the Command interface and adds a method for automatic execution.
+ *
+ * @param <T> is the type of validator used by the team.
+ */
 interface AutoCommand<T : Validator> : Command<T> {
+
+    /**
+     * Executes the command automatically with arguments passed.
+     *
+     * @param args Command line arguments.
+     */
     fun autoExecute(vararg args: String?)
 }
--- src/main/kotlin/org/example/kotlincollectionmanager/command/interfaces/Validator.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/command/interfaces/Validator.kt
@@ -1,5 +1,16 @@
 package org.example.kotlincollectionmanager.command.interfaces
 
+/**
+ * Interface for validating command arguments.
+ * Defines a contract for verifying the correctness of the input data before executing the command.
+ */
 interface Validator {
+
+    /**
+     * Checks the arguments of the command for compliance with the requirements.
+     *
+     * @param args List of arguments for validation.
+     * @param command is the command for which the arguments are checked.
+     */
     fun validateArgs(args: List<String>, command: Command<out Validator>)
 }
