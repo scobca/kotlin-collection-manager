81587285136a9fb201bb5cc78c45b654d2bf3d26
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-11 19:09:00 +0300

    docs(javadoc): described command storage & validators

--- src/main/kotlin/org/example/kotlincollectionmanager/command/validators/NoArgsCommandValidator.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/command/validators/NoArgsCommandValidator.kt
@@ -4,8 +4,18 @@ import org.example.kotlincollectionmanager.command.interfaces.Command
 import org.example.kotlincollectionmanager.command.interfaces.Validator
 import org.springframework.stereotype.Component
 
+/**
+ * Validator for commands that do not accept arguments.
+ * Checks for the absence of arguments and executes the command if there are no arguments.
+ */
 @Component
 class NoArgsCommandValidator : Validator {
+    /**
+     * Checks the list of command arguments and executes the command if there are no arguments.
+     *
+     * @param args List of command arguments.
+     * @param command is the command for which the arguments are checked.
+     */
     override fun validateArgs(args: List<String>, command: Command<out Validator>) {
         return if (args.isEmpty()) {
             command.execute()
--- src/main/kotlin/org/example/kotlincollectionmanager/command/validators/config/ValidatorsConfig.kt
+++ src/main/kotlin/org/example/kotlincollectionmanager/command/validators/config/ValidatorsConfig.kt
@@ -7,23 +7,48 @@ import org.example.kotlincollectionmanager.command.validators.OneOrTwoArgsValida
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/**
+ * Configuration class for creating and registering command validators in the application context.
+ */
 @Configuration
 class ValidatorConfig {
+
+    /**
+     * Creates and registers a validator bin for commands without arguments.
+     *
+     * @return Instance of NoArgsCommandValidator.
+     */
     @Bean
     fun noArgsCommandValidator(): NoArgsCommandValidator {
         return NoArgsCommandValidator()
     }
 
+    /**
+     * Creates and registers a validator bin for commands with one argument.
+     *
+     * @return Instance of OneArgCommandValidator.
+     */
     @Bean
     fun oneArgCommandValidator(): OneArgCommandValidator {
         return OneArgCommandValidator()
     }
 
+
+    /**
+     * Creates and registers a validator bin for commands with one or two arguments.
+     *
+     * @return Instance of OneOrTwoArgsValidator.
+     */
     @Bean
     fun oneOrTwoArgsValidator(): OneOrTwoArgsValidator {
         return OneOrTwoArgsValidator()
     }
 
+    /**
+     * Creates and registers a validator bin for the help command.
+     *
+     * @return An instance of HelpCommandValidator.
+     */
     @Bean
     fun helpCommandValidator(): HelpCommandValidator {
         return HelpCommandValidator()
